fc1bab3efd1126bc9892fb9a7cca7208916c8e12
sachin <sachinnagar2004@gmail.com>
2025-05-28 04:16:51 +0530

    favicon and description added

--- app/layout.js
+++ app/layout.js
@@ -16,13 +16,20 @@ import {
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "Eve'sBoutique",
-  description: "E-Commerce with Next.js ",
+  title: "Eve's Boutique",
+  description: "Discover Eve's Boutique — your ultimate destination for trendy fashion, curated collections, and seamless shopping.",
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
+        <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+
         <body className={`${outfit.className} antialiased text-gray-700`} >
         <ClerkProvider>
           <Toaster />
