cc6ad2fbfc177b2cf8a2075ca773e1c5bbdb5e13
sachin <sachinnagar2004@gmail.com>
2025-05-28 03:09:12 +0530

    update feature added

--- app/api/order/create/function.js
+++ app/api/order/create/function.js
@@ -39,6 +39,7 @@ ${enrichedItems.map((item, i) =>
         const token = process.env.TOKEN; // keep this secure
         const phoneNumberId = '677332382129481';
        const recipientPhone = '+916375921418';
+       
     //    const recipientPhone = '+917005969269';
 
         // Send message
